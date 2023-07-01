0ff6261366ac1db6ee518596515c8d2520ff2026
Shreyas <shreyas.misra03@gmail.com>
2023-07-01 13:08:40 +0530

    Bug - Dates not getting selected porperly gonna fix it!

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,7 +1,51 @@
 "use client";
 
-const ListingReservation = () => {
-  return <div></div>;
+import { Range } from "react-date-range";
+import Calender from "../inputs/Calender";
+
+interface ListingReservationProps {
+  price: number;
+  totalPrice: number;
+  onChangeDate: (value: Range) => void;
+  dateRange: Range;
+  onSubmit: () => void;
+  disabled: boolean;
+  disabledDates: Date[];
+}
+
+const ListingReservation: React.FC<ListingReservationProps> = ({
+  price,
+  totalPrice,
+  onChangeDate,
+  dateRange,
+  onSubmit,
+  disabled,
+  disabledDates,
+}) => {
+  return (
+    <div
+      className="
+    bg-white
+    rounded-xl
+    border-[1px]
+    border-neutral-200
+    overflow-hidden
+  "
+    >
+      <div className="flex flex-row items-center gap-1 p-4">
+        <span className="text-md font-semibold">$ {price}</span>
+        <span className="text-xs text-neutral-600">/ nights</span>
+      </div>
+
+      <hr />
+
+      <Calender
+        value={dateRange}
+        disabledDates={disabledDates}
+        onChange={(value) => onChangeDate(value)}
+      />
+    </div>
+  );
 };
 
 export default ListingReservation;
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -4,6 +4,7 @@ import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingImage from "@/app/components/listings/ListingImage";
 import ListingInfo from "@/app/components/listings/ListingInfo";
+import ListingReservation from "@/app/components/listings/ListingReservation";
 import { categories } from "@/app/components/navbar/Categories";
 import useLoginModal from "@/app/hooks/useLoginModal";
 import { SafeListing, SafeUser } from "@/app/types";
@@ -129,6 +130,18 @@ const ListingClient: React.FC<ListingClientProps> = ({
                 title={listing.title}
                 category={category}
               />
+
+              <div className="order-first mb-10 md:order-last md:col-span-3">
+                <ListingReservation
+                  price={listing.price}
+                  totalPrice={totalPrice}
+                  onChangeDate={(value) => setDateRange(value)}
+                  dateRange={dateRange}
+                  onSubmit={onCreateReservation}
+                  disabled={isLoading}
+                  disabledDates={disabledDates}
+                />
+              </div>
             </div>
           </section>
         </div>
